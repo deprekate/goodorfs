d093bca4311cc2bf90186a5f08b10033e1a80452
deprekate <deprekate@gmail.com>
2020-12-23 17:05:21 -0800

    Adding C code to repo

---
+++ src/goodorfs.c
@@ -0,0 +1,22 @@
+/* 
+    goodorfs
+    Copyright (C) 2020 Katelyn McNair and Robert Edwards
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <Python.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <float.h>
+#include <getopt.h>
+#include "uthash.h"
